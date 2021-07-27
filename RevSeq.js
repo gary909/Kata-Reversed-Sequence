be287ad9b3867e04997b9059a7e9b94830760b09
gary <gary.white@lodgit.com>
2021-07-27 17:16:34 +0200

    code and things

--- RevSeq.js
+++ RevSeq.js
@@ -5,12 +5,23 @@
 //   };
 
 
+// function reverseSeq(n) {
+//     let text = "";
+//     for (let i = 0; i <= n; i++){
+//         text += "The number is " + i + "\n";
+//     }
+//     return text;
+//   };
+
 function reverseSeq(n) {
-    let text = "";
-    for (let i = 0; i <= n; i++){
-        text += "The number is " + i + "\n";
+    let arr = [];
+    let amount = n;
+    for (let i = 0; i < amount; i++){
+        arr.push(n);
+        n = n - 1;
     }
-    return text;
+    return arr;
   };
 
-console.log(reverseSeq(5)); // returns: [5, 4, 3, 2, 1]
+console.log(reverseSeq(7)); // returns: [5, 4, 3, 2, 1]
+
